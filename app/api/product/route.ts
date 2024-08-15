f2ad53badd5e0ad6d239386676122ca56295cdf9
amplil <aikotobahaaburakatabura@gmail.com>
2024-08-15 14:39:27 +0900

    product api

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,23 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   const headers={    // レスポンスヘッダー
       "Access-Control-Allow-Credentials":"false",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET",
       "Access-Control-Allow-Headers": "Content-Type",
     }
+  const params = request.nextUrl.searchParams;
+  const id = params.get("id");
+
+  if (!id) { // Check if id is null
+    return NextResponse.json(
+      { error: "ID is required" },
+      { 
+        status: 400,
+        headers: headers,
+      },
+    );
+  }
+  
   try{
-    const item_list = await prisma.product.findMany({
+    const item = await prisma.product.findUnique({
       where: { 
-        //userId: session?.user?.id,
-        tags: {some:{name:'コスメ'}},
+        id: id,
       },
     });
-    console.log({item_list})
+    console.log({item})
     return NextResponse.json(
-      item_list, 
+      item, 
       {
         status: 201,
         headers:headers,
@@ -26,7 +38,7 @@ export async function GET() {
   }
   catch {
     return NextResponse.json(
-      { error: "item search error" },
+      { error: "get item id error" },
       { 
         status: 400,
         headers:headers,
--- next.config.js
+++ next.config.js
@@ -12,20 +12,6 @@ const nextConfig = {
       }
     ]
   },
-  async headers() {
-    return [
-      {
-        source: '/api/item-search',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'false' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ]
-  },
-
 };
 
 module.exports = nextConfig;
